34cc0bc27ff83162f2fa3df507d306d273c6685b
Harsh1197 <harshbhardwaj728.hb@gmail.com>
2023-09-05 19:23:40 +0100

    Chat Changes

--- client/src/pages/Home/ChatPage.js
+++ client/src/pages/Home/ChatPage.js
@@ -3,7 +3,7 @@ import { PrettyChatWindow } from 'react-chat-engine-pretty';
 import { useSelector, useDispatch } from 'react-redux';
 import axios from 'axios';
 import { SetUser } from '../../redux/Users'; 
-
+import './App.css';
 const ChatsPage = () => {
     const user = useSelector(state => state.users);
     const dispatch = useDispatch();
@@ -35,7 +35,7 @@ const ChatsPage = () => {
                 projectId={process.env.REACT_APP_CHAT_ENGINE_PROJECT_ID}
                 username={user.user.name}
                 secret={user.user.name}
-                style={{ height: '80vh' }}
+                style={{ height: '180vh' }}
             />
         </div>
     );
